dfa8422e7df2434c2f85819617a7197956fd1917
ArielSB11 <arielsolisb@gmail.com>
2024-07-28 19:33:30 -0600

    Carrito primera version

---
+++ Tienda_Bazar/Controllers/CarritoController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Security.Claims;
+using Tienda_Bazar.Models;
+
+namespace Tienda_Bazar.Controllers
+{
+    public class CarritoController : Controller
+    {
+        private readonly BazarLibreriaContext _appDbContext;
+
+        public CarritoController(BazarLibreriaContext context)
+        {
+            _appDbContext = context;
+        }
+
+        public IActionResult ViewCarrito()
+        {
+            var userIdClaim = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+            var userId = int.Parse(userIdClaim);
+            var carritoItems = _appDbContext.CarritoCompras
+                                       .Where(c => c.CodigoUsuario == userId)
+                                       .Select(c => new CartItemViewModel
+                                       {
+                                           ProductoId = c.CodigoProducto,
+                                           ProductoNombre = c.Producto.NombreProducto,
+                                           Cantidad = c.Cantidad,
+                                           Precio = c.Producto.Precio,
+                                           Subtotal = c.Cantidad * c.Producto.Precio
+                                       })
+                                       .ToList();
+
+            var carritoViewModel = new CartViewModel
+            {
+                Items = carritoItems,
+                Total = carritoItems.Sum(i => i.Subtotal)
+            };
+
+            return View(carritoViewModel);
+        }
+    }
+}
---
+++ Tienda_Bazar/Models/CarritoViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Tienda_Bazar.Models
+{
+    public class CartItemViewModel
+    {
+        public int ProductoId { get; set; }
+        public string ProductoNombre { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Precio { get; set; }
+        public decimal Subtotal { get; set; }
+    }
+
+    public class CartViewModel
+    {
+        public List<CartItemViewModel> Items { get; set; } = new List<CartItemViewModel>();
+        public decimal Total { get; set; }
+    }
+}
